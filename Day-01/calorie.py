08f8a2da82aeb1aea4b1630cee4375c1adecddad
Callum Burgess <c13burgess@gmail.com>
2023-01-31 16:07:05 +0000

    Format Calorie Input

--- Day-01/calorie.py
+++ Day-01/calorie.py
@@ -1,4 +1,10 @@
 # https://adventofcode.com/2022/day/1
 
-def FindMostCalories(calories):
+def FindMostCalories(calorieList):
     return
+
+with open("input.txt", "r") as input:
+    calorieList = input.readlines()
+    calorieList = [a.strip("\n") for a in calorieList]
+print(calorieList)
+FindMostCalories(calorieList)

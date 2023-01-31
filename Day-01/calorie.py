a7599b9cd9536f57cf0c2f8c4b6afe8ad4727aaa
Callum Burgess <c13burgess@gmail.com>
2023-01-31 16:21:18 +0000

    Day 1 Part 1 Solution

--- Day-01/calorie.py
+++ Day-01/calorie.py
@@ -1,9 +1,21 @@
 # https://adventofcode.com/2022/day/1
 
 def FindMostCalories(calorieList):
-    return
+    currentElf = []
+    currentCalorie = ""
+    elfList = []
+
+    while(len(calorieList) != 0):
+        currentCalorie = calorieList.pop()
+        if currentCalorie != "":
+            currentElf.append(int(currentCalorie))
+            continue
+        elfList.append(sum(currentElf))
+        currentElf = []
+    elfList.sort(reverse=True)
+    return elfList[0]
 
 with open("input.txt", "r") as input:
     calorieList = input.readlines()
     calorieList = [a.strip("\n") for a in calorieList]
-FindMostCalories(calorieList)
+print(FindMostCalories(calorieList))

e90093f3464459fa574a757759e76a170eb0b975
Callum Burgess <c13burgess@gmail.com>
2023-02-01 13:54:09 +0000

    Score Dict and Result Dict

--- Day-02/rps.py
+++ Day-02/rps.py
@@ -4,3 +4,9 @@ with open("input.txt", "r") as input:
     rpsList = input.readlines()
     rpsList = [a.strip("\n").split(" ") for a in rpsList]
 print(rpsList)
+
+scoreDict = {"A" : 1, "B" : 2, "C" : 3,
+             "X" : 1, "Y" : 2, "Z" : 3}
+resultDict = {["A", "X"] : 3, ["A", "Y"] : 0, ["A", "Z"] : 6,
+              ["B", "X"] : 6, ["B", "Y"] : 3, ["B", "Z"] : 0,
+              ["C", "X"] : 0, ["C", "Y"] : 6, ["C", "Z"] : 3}

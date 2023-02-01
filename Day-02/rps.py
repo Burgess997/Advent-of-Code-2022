2e65e21717676f93f1a52abf4323de0701cb3872
Callum Burgess <c13burgess@gmail.com>
2023-02-01 13:46:30 +0000

    Format RPS Input

--- Day-02/rps.py
+++ Day-02/rps.py
@@ -1,1 +1,6 @@
 # https://adventofcode.com/2022/day/2
+
+with open("input.txt", "r") as input:
+    rpsList = input.readlines()
+    rpsList = [a.strip("\n").split(" ") for a in rpsList]
+print(rpsList)

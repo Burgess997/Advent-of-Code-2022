9de388ad6e99450d4f0d9a16a72daff972ef3b87
Callum Burgess <c13burgess@gmail.com>
2023-02-01 14:04:33 +0000

    Day 2 Part 1 Solution

--- Day-02/rps.py
+++ Day-02/rps.py
@@ -3,10 +3,19 @@
 with open("input.txt", "r") as input:
     rpsList = input.readlines()
     rpsList = [a.strip("\n").split(" ") for a in rpsList]
-print(rpsList)
 
-scoreDict = {"A" : 1, "B" : 2, "C" : 3,
-             "X" : 1, "Y" : 2, "Z" : 3}
-resultDict = {["A", "X"] : 3, ["A", "Y"] : 0, ["A", "Z"] : 6,
-              ["B", "X"] : 6, ["B", "Y"] : 3, ["B", "Z"] : 0,
-              ["C", "X"] : 0, ["C", "Y"] : 6, ["C", "Z"] : 3}
+scoreDict = {"X" : 1, "Y" : 2, "Z" : 3}
+
+# X/A = Rock
+# Y/B = Paper
+# Z/C = Scissors
+
+resultDict = {"AX" : 3, "AY" : 6, "AZ" : 0,
+              "BX" : 0, "BY" : 3, "BZ" : 6,
+              "CX" : 6, "CY" : 0, "CZ" : 3}
+
+def rpsScoreCalculator(strategyGuide):
+    score = [scoreDict[a[1]] + resultDict["".join(a)] for a in strategyGuide]
+    return sum(score)
+
+print(rpsScoreCalculator(rpsList))
